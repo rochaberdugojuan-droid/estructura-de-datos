02e1a8dbb652f26d0d94d57448cefd9a0fd876f3
rochaberdugojuan-droid <rochaberdugojuan@gmail.com>
2026-09-18 13:28:42 -0500

    Add student array and print loop
    Added a string array of students and a loop to print their indices.

--- vector.cs
+++ vector.cs
@@ -14,7 +14,14 @@ namespace ConsoleApp1
             int[] numeros = new int[5]
                 { 25, 50 , 100, 200, 400 };
             Console.WriteLine($"Elementos en [0]: {numeros[0]}");
-        }
 
+            string[] estudiantes =
+                { "juan","fernando","rocha","berdugo"};
+
+            for (int i = 0; i < estudiantes.Length; i++)
+            {
+                Console.WriteLine($"indice [{i}]:{estudiantes[i]}");
+            }
+        }
     }
 }
