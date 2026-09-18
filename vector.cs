84dcfa6e2c383a494a8dc197fce502b8e5e880aa
rochaberdugojuan-droid <rochaberdugojuan@gmail.com>
2026-09-18 13:11:51 -0500

    Add vector.cs with initial array and output

---
+++ vector.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numeros = new int[5]
+                { 25, 50 , 100, 200, 400 };
+            Console.WriteLine($"Elementos en [0]: {numeros[0]}");
+        }
+
+    }
+}
